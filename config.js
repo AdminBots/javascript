f9b3feb6083b3652da784575a64fca57998f5ca4
AdminBots <mohamad.html.70@gmail.com>
2025-03-27 16:35:43 +0330

    Rename config.js to source/config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-export const TOKEN = "";
-export const BASE_URL = `https://tapi.bale.ai/bot${TOKEN}`;
---
+++ source/config.js
@@ -0,0 +1,2 @@
+export const TOKEN = "";
+export const BASE_URL = `https://tapi.bale.ai/bot${TOKEN}`;
